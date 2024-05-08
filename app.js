1951a456630f769ad25fd64f992822a4b657c190
Ivanwell <lviv08@gmail.com>
2024-05-08 14:48:17 +0300

    first commit2

--- app.js
+++ app.js
@@ -1,20 +1,21 @@
-const PORT = 3300;
-const express = require("express");
-const cors = require("cors");
-const mongoose = require("mongoose");
+const PORT = 3300
+const express = require('express')
+const cors = require('cors')
+const mongoose = require('mongoose')
 
-const app = express();
-app.use(express.json());
-app.use(cors());
+console.log('hello')
 
-mongoose
-    .connect(
-      "mongodb+srv://lviv08:EcTGNgmPF6HcKdQ9@database.atog457.mongodb.net/Back_end_bayrakparts?retryWrites=true&w=majority"
-    )
+const app = express()
+app.use(express.json())
+app.use(cors())
+
+mongoose.connect(
+  'mongodb+srv://lviv08:EcTGNgmPF6HcKdQ9@database.atog457.mongodb.net/Back_end_bayrakparts?retryWrites=true&w=majority'
+)
 
 require('./routes').Api(app)
 require('./routes').Admin(app)
 
-require('./scripts/index.js')();
+require('./scripts/index.js')()
 
-const server = app.listen(3300, () => console.log("server is alive"));
+const server = app.listen(3300, () => console.log('server is alive'))
